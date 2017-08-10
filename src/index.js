be82d32696b58d023f4cba1298ebf70587670317
Habib Ridho <habibridho@live.com>
2017-08-10 14:00:03 +0700

    fix import bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import AmfTextInput from './AmfTextInput/AmfTextInput'
-import AmfNumberInput from './AmfTextInput/AmfNumberInput'
+import AmfInputNumber from './AmfInputNumber/AmfInputNumber'
 import AmfDropdown from './AmfDropdown/AmfDropdown'
 import AmfDatepicker from './AmfDatepicker/AmfDatepicker'
 import AmfDateText from './AmfDateText/AmfDateText'
 
-export { AmfTextInput, AmfNumberInput, AmfDropdown, AmfDatepicker, AmfDateText }
+export { AmfTextInput, AmfInputNumber, AmfDropdown, AmfDatepicker, AmfDateText }
