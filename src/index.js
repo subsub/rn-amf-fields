20922344b9444cdba00efc1a3b0cc4161e702a6f
Egon Firman <egon.firman@gmail.com>
2017-08-10 23:00:27 +0700

    fixing adding AmfField

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import AmfNumberInput from './AmfNumberInput/AmfNumberInput'
 import AmfDropdown from './AmfDropdown/AmfDropdown'
 import AmfDatepicker from './AmfDatepicker/AmfDatepicker'
 import AmfDateText from './AmfDateText/AmfDateText'
+import AmfField from './AmfField/AmfField'
 
-export { AmfTextInput, AmfNumberInput, AmfDropdown, AmfDatepicker, AmfDateText }
+export { AmfTextInput, AmfNumberInput, AmfDropdown, AmfDatepicker, AmfDateText, AmfField }
