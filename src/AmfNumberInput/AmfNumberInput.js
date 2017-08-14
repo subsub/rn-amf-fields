c033987f0513be55952226326a3491e627421c62
Egon Firman <egon.firman@gmail.com>
2017-08-14 13:47:09 +0700

    Removing validation

--- src/AmfNumberInput/AmfNumberInput.js
+++ src/AmfNumberInput/AmfNumberInput.js
@@ -1,119 +1,34 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { StyleSheet, TextInput, Text, View } from 'react-native'
+import fieldPropTypes from '../fieldPropTypes'
 import { TextField } from 'react-native-material-textfield'
-import numeral from 'numeral'
-
-/**
- * sorry fo the hardcode
- *
- */
 
 class AmfNumberInput extends Component {
-  constructor(props) {
-    super(props)
 
-    this.state = {
-      valid: false,
-      message: ''
-    }
-  }
-
-  componentDidMount() {
-    this.validate()
-  }
-
-  onBlur = () => {
-    this.validate()
-  }
-
-  onFocus = () => {
-    if (this.props.validation && this.props.validation.required && this.props.value === null) this.props.onChangeValue(this.props.pristineValue)
-  }
-
-  inputValid = () => {
-    const { validation, label, pristineValue } = this.props
-    if (!validation) return ({status: true})
-
-    // check if pristine
-    if (this.props.value === null || this.props.value === undefined) return ({status: true})
-
-    // check whether input is required
-    if (validation.required) {
-      if (this.props.value === pristineValue) return ({status: false, message: label + ' tidak boleh kosong'})
-    }
-
-    return ({status: true})
-  }
-
-  validate = () => {
-    let validationObject = this.inputValid()
-    this.setState({valid: validationObject.status, errorMessage: validationObject.message})
-  }
+	onChange = val => this.props.onChange(val)
 
   render() {
-    let textInputValidationStyle = {}
-    let validationText
 
-    const { label, value, onChange } = this.props
+    const { label, value, error } = this.props
 
-    if (!this.state.valid) {
-      textInputValidationStyle = { borderColor: 'red' }
-      validationText = <Text style={style.errorText}>{this.state.errorMessage}</Text>
-    }
+		const { onChange } = this;
 		
     return (
-      <View>
-        <TextField
-          label={label}
-          onChangeText={onChange}
-          keyboardType="numeric"
-          returnKeyType="next"
-          value={value || ''}
-        />
-        { validationText }
-      </View>
+			<TextField
+				label={label}
+				onChangeText={onChange}
+				keyboardType="numeric"
+				returnKeyType="next"
+				value={value || ''}
+				error={error}
+			/>
     )
   }
 }
 
-const style = StyleSheet.create({
-  errorText: {
-    color: 'red',
-    margin: 4,
-    marginBottom: 0
-  },
-  label: {
-    color: '#454545',
-    marginBottom: 8
-  },
-  textInput: {
-    backgroundColor: 'white',
-    height: 40,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#DDDDDD',
-    color: '#9B9B9B',
-    fontSize: 14,
-    borderRadius: 3,
-    paddingBottom:10
-  }
-})
-
-AmfNumberInput.defaultProps = {
-  pristineValue: -1
-}
-
 AmfNumberInput.propTypes = {
-  delimiter: PropTypes.string,
-  label: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  precision: PropTypes.number,
-  pristineValue: PropTypes.number,
-  separator: PropTypes.string,
-  style: TextInput.propTypes.style,
-  unit: PropTypes.string,
-  value: PropTypes.number
+	...fieldPropTypes,
+	value : PropTypes.string
 }
 
 export default AmfNumberInput
--- src/AmfTextInput/AmfTextInput.test.js
+++ src/AmfTextInput/AmfTextInput.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import AmfTextInput from './AmfTextInput'
 import renderer from 'react-test-renderer'
 
-test('AmfInputText without validation', () => {
+test('AmfInputText simple render', () => {
   let props = {
     label: 'Label',
     onChange: () => {},
@@ -15,191 +15,15 @@ test('AmfInputText without validation', () => {
   expect(tree).toMatchSnapshot()
 })
 
-test('AmfInputText with required validation', () => {
+test('AmfInputText simple render with error', () => {
   let props = {
     label: 'Label',
     onChange: () => {},
-    value: null,
-    validation: {
-      required: true
-    }
-  }
-
-  const component = renderer.create(<AmfTextInput {...props} />)
-  const componentError = renderer.create(<AmfTextInput {...props} value="" />)
-  const componentErrorAB = renderer.create(<AmfTextInput {...props} value="ab" />)
-
-  let tree = component.toJSON()
-  let treeError = componentError.toJSON()
-  let treeErrorAB = componentErrorAB.toJSON()
-  expect(tree).toMatchSnapshot()
-  expect(treeError).toMatchSnapshot()
-  expect(treeErrorAB).toMatchSnapshot()
-})
-
-test('AmfInputText with regex validation', () => {
-  let props = {
-    label: 'Label',
-    onChange: () => {},
-    value: null,
-    validation: {
-      regex: /e/
-    }
-  }
-  let propsNotNull = {
-    label: 'Label',
-    onChange: () => {},
-    value: '',
-    validation: {
-      regex: /e/
-    }
-  }
-  let propsRight = {
-    label: 'Label',
-    onChange: () => {},
-    value: 'test',
-    validation: {
-      regex: /e/
-    }
-  }
-  let propsWrong = {
-    label: 'Label',
-    onChange: () => {},
-    value: 'ups',
-    validation: {
-      regex: /e/
-    }
-  }
-
-  const component = renderer.create(<AmfTextInput {...props} />)
-  const componentNotNull = renderer.create(<AmfTextInput {...propsNotNull} />)
-  const componentRight = renderer.create(<AmfTextInput {...propsRight} />)
-  const componentWrong = renderer.create(<AmfTextInput {...propsWrong} />)
-
-  let tree = component.toJSON()
-  let treeNotNull = componentNotNull.toJSON()
-  let treeRight = componentRight.toJSON()
-  let treeWrong = componentWrong.toJSON()
-  expect(tree).toMatchSnapshot()
-  expect(treeNotNull).toMatchSnapshot()
-  expect(treeRight).toMatchSnapshot()
-  expect(treeWrong).toMatchSnapshot()
-})
-
-test('AmfInputText with minLength validation', () => {
-  let props = {
-    label: 'Label',
-    onChange: () => {},
-    value: null,
-    validation: {
-      minLength: 2
-    }
-  }
-
-  const component = renderer.create(<AmfTextInput {...props} />)
-  const componentError = renderer.create(<AmfTextInput {...props} value="" />)
-  const componentErrorAB = renderer.create(<AmfTextInput {...props} value="ab" />)
-
-  let tree = component.toJSON()
-  let treeError = componentError.toJSON()
-  let treeErrorAB = componentErrorAB.toJSON()
-  expect(tree).toMatchSnapshot()
-  expect(treeError).toMatchSnapshot()
-  expect(treeErrorAB).toMatchSnapshot()
-})
-
-test('AmfInputText with exactLength validation', () => {
-  let props = {
-    label: 'Label',
-    onChange: () => {},
-    value: null,
-    validation: {
-      exactLength: 2
-    }
-  }
-
-  const component = renderer.create(<AmfTextInput {...props} />)
-  const componentError = renderer.create(<AmfTextInput {...props} value="" />)
-  const componentErrorAB = renderer.create(<AmfTextInput {...props} value="ab" />)
-  const componentErrorABC = renderer.create(<AmfTextInput {...props} value="abC" />)
-
-  let tree = component.toJSON()
-  let treeError = componentError.toJSON()
-  let treeErrorAB = componentErrorAB.toJSON()
-  let treeErrorABC = componentErrorABC.toJSON()
-  expect(tree).toMatchSnapshot()
-  expect(treeError).toMatchSnapshot()
-  expect(treeErrorAB).toMatchSnapshot()
-  expect(treeErrorABC).toMatchSnapshot()
-})
-
-test('AmfInputText with maxLength validation', () => {
-  let props = {
-    label: 'Label',
-    onChange: () => {},
-    value: null,
-    validation: {
-      maxLength: 2
-    }
-  }
-
-  const component = renderer.create(<AmfTextInput {...props} />)
-  const componentError = renderer.create(<AmfTextInput {...props} value="" />)
-  const componentErrorAB = renderer.create(<AmfTextInput {...props} value="ab" />)
-  const componentErrorABC = renderer.create(<AmfTextInput {...props} value="abC" />)
-
-  let tree = component.toJSON()
-  let treeError = componentError.toJSON()
-  let treeErrorAB = componentErrorAB.toJSON()
-  let treeErrorABC = componentErrorABC.toJSON()
-  expect(tree).toMatchSnapshot()
-  expect(treeError).toMatchSnapshot()
-  expect(treeErrorAB).toMatchSnapshot()
-  expect(treeErrorABC).toMatchSnapshot()
-})
-
-test('AmfInputText onFocus', () => {
-  let props = {
-    label: 'Label',
-    onChange: jest.fn(),
-    value: 'test',
-    validation: {
-      required: true
-    }
-  }
-  let propsPristine = {
-    label: 'Label',
-    onChange: jest.fn(),
-    value: null,
-    validation: {
-      required: true
-    }
-  }
-  let propsNull = {
-    label: 'Label',
-    onChange: jest.fn(),
     value: null
   }
 
-  // const component = renderer.create(<AmfTextInput {...props} />)
-  const component = new AmfTextInput(props)
-  const componentPristine = new AmfTextInput(propsPristine)
-  const componentWithoutValidation = new AmfTextInput(propsNull)
+  const component = renderer.create(<AmfTextInput {...props} />)
 
-  component.onFocus()
-  componentPristine.onFocus()
-  componentWithoutValidation.onFocus()
-
-  expect(props.onChange).not.toHaveBeenCalled()
-  expect(propsPristine.onChange).toHaveBeenCalled()
-  expect(propsNull.onChange).not.toHaveBeenCalled()
-})
-
-test('AmfInputText onBlur', () => {
-  const component = new AmfTextInput({})
-
-  component.validate = jest.fn()
-  component.onBlur()
-
-  expect(component.validate).toHaveBeenCalled()
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
 })
