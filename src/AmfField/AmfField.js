943570478c0697b1d0cd350d9d9811210f06c3fd
Egon Firman <egon.firman@gmail.com>
2017-08-22 09:27:09 +0700

    Adding error to AmfField

--- src/AmfField/AmfField.js
+++ src/AmfField/AmfField.js
@@ -32,16 +32,15 @@ class AmfField extends Component {
   }
 
   render() {
-    const { type, label, onChange, value, options  } = this.props
+    const { type, label, onChange, value, options, error } = this.props
     if (type in switcher) {
       const Component = switcher[type]
-      return <Component type={type} label={label} onChange={onChange} value={value} options={options} />
+      return <Component type={type} label={label} onChange={onChange} value={value} options={options} error={error} />
     } else {
       throw new TypeError ('Unknown type Field "' + type + '" in AmfField')
     }
   }
 }
-
 
 AmfField.propTypes = {...fieldPropTypes, type:PropTypes.string.isRequired}
 
