e1d615d8598e1f269e90997e001b7268d10c2e8e
egon12 <egon.firman@gmail.com>
2017-10-10 13:14:22 +0700

    Adding <= in AmarthaDateInpuField

--- src/AmfDateInput/AmfDateInput.js
+++ src/AmfDateInput/AmfDateInput.js
@@ -128,7 +128,7 @@ const months = ['MM', 'Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Ags', 'S
 const days   = ['DD', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
 
 let yearsConst = ['YYYY']
-for (let i=1940; i< new Date().getFullYear() - 18; i++) { yearsConst.push(String(i)) }
+for (let i=1940; i <= new Date().getFullYear() - 18; i++) { yearsConst.push(String(i)) }
 const years = yearsConst
 
 
